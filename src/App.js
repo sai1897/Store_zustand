0c79a4e1de43f18d6f8341450db1b77f624c4742
sainath <sainathp@cogncise.com>
2023-08-22 09:38:59 +0530

    gg

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import { createContext } from "react";
+import { createContext, useEffect } from "react";
 import Child from "./Child"
 import useBearStore from "./Store";
 
 
 
 const App=()=> {
+  useEffect(()=>{
+    localStorage.setItem("val","value")
+  })
   function BearCounter() {
     const bears = useBearStore((state) => state.bears)
     return <h1>{bears} around here ...</h1>
--- src/Store.js
+++ src/Store.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { create } from 'zustand'
 
     const useBearStore = create((set) => ({
-        bears: 100,
+        bears: (localStorage.getItem("val")),
         increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
         removeAllBears: () => set({ bears: 0 }),
       }))
